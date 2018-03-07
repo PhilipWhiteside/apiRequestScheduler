b426dd2e203e88ead02130707adc23331e368756
Philip Whiteside <pwsubbox@gmail.com>
2018-03-07 11:32:24 +0000

    Add config struct

--- main.go
+++ main.go
@@ -5,6 +5,18 @@ import (
 	"os"
 )
 
+// Config for user configuraiton used throughout
+type Config struct {
+	ConfigVersion            int
+	AppDebug                 bool
+	ServerAPIPath            string
+	ServerAPIUser            string
+	ServerAPIPass            string
+	ServerAPIPayloadDefault  string
+	ServerAPIIgnoreCertError bool
+	ICALPath                 string
+}
+
 func main() {
 	fmt.Println("\n------------------------------\nMain() - Starting application")
 	// READ command-line arguments to get config file path
