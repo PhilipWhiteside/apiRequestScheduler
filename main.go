b3b39e41197864cfbd07f6d57464590ccba094bf
Philip Whiteside <pwsubbox@gmail.com>
2018-03-07 11:40:34 +0000

    Add LoadConfiguration into Config struct

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 )
@@ -22,10 +23,32 @@ func main() {
 	// READ command-line arguments to get config file path
 	CliArguments := os.Args[1:]
 	fmt.Println("Main() - CLI arguments loaded -", CliArguments)
+	// Pass first CLI arugment as config file location
+	config := LoadConfiguration(CliArguments[0])
+	// Print config to stop "declared and not used error"
+	fmt.Println(config)
 	fmt.Println("Main() - Ending application")
 }
 
 // LoadConfiguration file then return Config struct
+func LoadConfiguration(filename string) Config {
+	fmt.Println("\nLoadConfiguration() - Loading config")
+	fmt.Println("LoadConfiguration() - Using config file -", filename)
+	var config Config
+	configFile, err := os.Open(filename)
+	defer configFile.Close()
+	if err != nil {
+		fmt.Println("ERROR: LoadConfiguration() - Configuration file loading has ERRORS")
+		fmt.Println(err.Error())
+	} else {
+		fmt.Println("LoadConfiguration() - Config file has loaded with NO errors")
+	}
+	jsonParser := json.NewDecoder(configFile)
+	jsonParser.Decode(&config)
+	fmt.Println("LoadConfiguration() - Loaded -", config)
+	return config
+}
+
 // LoadICAL and return current event summary ON/OFF
 // Check ICAL current event payload
 // SetServerState power state from desired state
