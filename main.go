c0d9697d69292e1433b98537bb7039e5d7ab02e4
Philip Whiteside <pwsubbox@gmail.com>
2018-03-07 11:27:17 +0000

    Add CLI argument input for config file location

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
 	fmt.Println("\n------------------------------\nMain() - Starting application")
+	// READ command-line arguments to get config file path
+	CliArguments := os.Args[1:]
+	fmt.Println("Main() - CLI arguments loaded -", CliArguments)
 	fmt.Println("Main() - Ending application")
 }
 
